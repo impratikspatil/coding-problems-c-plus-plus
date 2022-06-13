0553750fec9c3c6c3a16d232a6d867a0a40c1fff
impratikspatil <impratikspatil@gmail.com>
2022-06-14 01:08:13 +0530

    .

---
+++ equal_numbers_using_XOR.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+int main()
+{
+   int a,b,c;
+   a=10;
+   b=10;
+   c=30;
+
+   if((a^b)==0)
+   {
+    cout<<"a and b are equal numbers"<<endl;
+   }
+   else{
+    cout<<"a is not equal to b"<<endl;
+   }
+
+
+
+    if((b^c)==0)
+   {
+    cout<<"b and c are equal numbers"<<endl;
+   }
+   else{
+    cout<<"b is not equal to c"<<endl;
+   }
+
+   
+}
